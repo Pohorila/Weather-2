324f855a21e7f2cb7171e8942c078e55a8bed65c
Dariia Pohorila <orestivna@ukr.net>
2022-09-07 18:38:03 +0100

    added forecast to js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,33 @@ function showCity(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
+function showForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-sm-2">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Mon</h5>
+                <p class="card-text">22°С</p>
+                <br />
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+              </div>
+            </div>
+          </div> `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
 let now = new Date();
 let h2 = document.querySelector("h2");
 let hour = now.getHours();
@@ -41,3 +68,4 @@ let day = days[now.getDay()];
 h2.innerHTML = `${day} ${hour}:${min}`;
 let cityForm = document.querySelector("#type-city");
 cityForm.addEventListener("submit", showCity);
+showForecast();
